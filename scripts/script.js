be6df635105a95f2eba225d9fedc1bc991138cdc
RobertB512 <rwbcool@gmail.com>
2022-12-07 11:36:09 -0800

    Change font, change button text, add some javascript variables and hide game by default, other small changes

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+let form = document.querySelector("form");
+let input = document.getElementById("guessed-number")
+let guessBtn = document.querySelector(".guess.btn")
+let newGameBtn = document.querySelector(".new-game-btn")
+let guessCountArea = document.querySelector(".guess-count-area")
+let timesGuessed = document.querySelector(".times-guessed")
+let guessesLeft = document.querySelector(".guesses-left")
