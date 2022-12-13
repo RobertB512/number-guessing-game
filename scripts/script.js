62c839fc699e3c97576fede63c0a537f58a366e6
RobertB512 <rwbcool@gmail.com>
2022-12-13 14:39:22 -0800

    Remove guess-count box and add games-played box, change css and js to reflect,change computer number to 1-100 and add js guess limit and game over screen

--- scripts/script.js
+++ scripts/script.js
@@ -4,22 +4,24 @@ let input = document.querySelector("#guessed-number");
 let message = document.querySelector(".message")
 let guessBtn = document.querySelector(".guess.btn");
 let newGameBtn = document.querySelector(".new-game-btn");
-let guessCountArea = document.querySelector(".guess-count-area");
-let timesGuessed = document.querySelector(".times-guessed");
+let statsArea = document.querySelector(".stats-area");
 let guessesLeft = document.querySelector(".guesses-left");
+// let gamesWonField = document.querySelector(".games-played");
+// let gamesWon = 0;
 let computerNumber;
-// function getComputerNumber() {
-//   let computerNumber = Math.floor(Math.random() * 26);
-//   return computerNumber
-// }
+let guesses = 10;
+
 
 // main game function
 function newGame() {
   // show/reset game elements at start
   heading.textContent = "Guess my number"
   numberEntryArea.style.display = "flex";
-  guessCountArea.style.display = "flex";
-  computerNumber = Math.floor(Math.random() * 26);
+  message.textContent = "Pick a number from 1 to 100"
+  statsArea.style.display = "flex";
+  computerNumber = Math.floor(Math.random() * 100) + 1;
+  guesses = 10
+  guessesLeft.textContent = String(guesses);
 }
 
 function guessNumber() {
@@ -30,15 +32,26 @@ function guessNumber() {
   } else if (playerNumber === computerNumber) {
     heading.textContent = "YOU WIN"
     numberEntryArea.style.display = "none";
-    guessCountArea.style.display = "none";
+    statsArea.style.display = "none";
     input.value = "";
     message.textContent = "";
   } else {
     message.textContent = `${playerNumber} is to hight`
   }
+  guesses--
+  guessesLeft.textContent = String(guesses);
+  if (guesses === 0) {
+    heading.textContent = "GAME OVER"
+    numberEntryArea.style.display = "none";
+    statsArea.style.display = "none";
+    input.value = "";
+    message.textContent = "";
+  }
+  
 
   console.log(`playerNumber: ${playerNumber}`)
   console.log(`computerNumber: ${computerNumber}`)
+  console.log(`guesses left ${guesses}`)
   console.log(`typeof computerNumber: ${typeof computerNumber}`)
   console.log(`input: ${input}`)
   console.log(`typeof playerNumber ${typeof playerNumber}`)
@@ -46,5 +59,3 @@ function guessNumber() {
 }
 
 
-// TODO
-// 1. Change form to just a single number input and button. This will prevent the page form refreshing, and might fix some other issues too.
