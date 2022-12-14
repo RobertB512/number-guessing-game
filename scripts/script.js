8b6e99f2fb4fc2f6bb916137cf30afc243243e57
RobertB512 <rwbcool@gmail.com>
2022-12-13 16:19:30 -0800

    Only take numbers from 1 to 100, make stats filds able to grow in lengh

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let heading = document.querySelector("h1");
 let numberEntryArea = document.querySelector(".number-entry-area");
-let input = document.querySelector("#guessed-number");
+let input = document.getElementById("guessed-number");
 let message = document.querySelector(".message")
 let guessBtn = document.querySelector(".guess.btn");
 let newGameBtn = document.querySelector(".new-game-btn");
@@ -13,39 +13,43 @@ let computerNumber;
 let guesses = 10;
 
 
+
 // main game function
 function newGame() {
   // show/reset game elements at start
   ++gamesPlayed
-  heading.textContent = "Guess my number"
-  numberEntryArea.style.display = "flex";
-  message.textContent = "Pick a number from 1 to 100"
-  statsArea.style.display = "flex";
   computerNumber = Math.floor(Math.random() * 100) + 1;
-  guesses = 10
+  heading.textContent = "Guess my number";
+  numberEntryArea.style.display = "flex";
+  message.textContent = `Pick a number from 1 to 100`;
+  statsArea.style.display = "flex";
+  guesses = 10;
   guessesLeft.textContent = String(guesses);
-  gamesPlayedField.textContent = `${gamesWon}/${gamesPlayed}`
+  gamesPlayedField.textContent = `${gamesWon}/${gamesPlayed}`;
 }
 
 function guessNumber() {
   let playerNumber = parseInt(input.value);
-
-  if (playerNumber < computerNumber) {
+  if (isNaN(playerNumber) || playerNumber < 1 || playerNumber > 100) {
+    message.textContent = "Pick a number from 1 to 100"
+  } else if (playerNumber < computerNumber) {
     message.textContent = `${playerNumber} is too low`
+    guesses--;
   } else if (playerNumber === computerNumber) {
-    gamesWon++
-    heading.textContent = "YOU WIN"
+    gamesWon++;
+    heading.textContent = "YOU WIN";
     numberEntryArea.style.display = "none";
     statsArea.style.display = "none";
     input.value = "";
     message.textContent = "";
+    guesses--;
   } else {
-    message.textContent = `${playerNumber} is to hight`
+    message.textContent = `${playerNumber} is to hight`;
+    guesses--;
   }
-  guesses--
   guessesLeft.textContent = String(guesses);
   if (guesses === 0) {
-    heading.textContent = "GAME OVER"
+    heading.textContent = "GAME OVER";
     numberEntryArea.style.display = "none";
     statsArea.style.display = "none";
     input.value = "";
